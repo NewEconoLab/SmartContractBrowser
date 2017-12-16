fcb61bd2dde22a409e84ced426473d27b87cf23c
lights <lightsever@hotmail.com>
2017-12-16 22:21:04 +0800

    add websocket project

---
+++ SmartContractBrowser/compilerServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace compilerServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            WebSocketSharp.Server.HttpServer server = new WebSocketSharp.Server.HttpServer(227);
+            server.Start();
+            server.DocumentRootPath = "abc";
+            server.OnGet += Server_OnGet;
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+       
+        private static async void Server_OnGet(object sender, WebSocketSharp.Server.HttpRequestEventArgs e)
+        {
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes("hello everywhere.");
+            e.Response.ContentEncoding = System.Text.Encoding.UTF8;
+            e.Response.ContentType = "application/json";
+            e.Response.ContentLength64 = buffer.Length;
+            e.Response.StatusCode = 200;
+            await e.Response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+            //await e.Response.OutputStream.FlushAsync();
+             e.Response.Close();
+        }
+    }
+}
